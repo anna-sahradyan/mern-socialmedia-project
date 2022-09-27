c115825a6018d48237b9dc70e6594e3b83a4bf92
anna <a.sahradyan72@gmail.com>
2022-09-28 00:39:37 +0400

    create client directory

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {AppBar, Container, Grid, Grow, Typography} from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import useStyles from "./style";
+
+const App = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <Container maxWidth={"lg"}>
+                <AppBar position={"static"} color={"inherit"} className={classes.appBar}>
+                    <Typography variant={"h2"} align={"center"} className={classes.heading}>Memories</Typography>
+                    <img src={"/images/memories3.png"} height={70} width={70} alt={"memories"} className={classes.image}/>
+                </AppBar>
+                <Grow in>
+                    <Container>
+                        <Grid container justify={"space-between"} alignItems={"stretch"} spacing={3}>
+
+                            <Grid item xs={12} sm={7}>
+                                <Posts/>
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                <Form/>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
+        </>
+    );
+};
+
+export default App;
